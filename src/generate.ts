bb359a3f06a4982ff7c6bdba1002bd6dd0a55bb4
Julian Popescu <jpopesculian@gmail.com>
2018-10-01 21:56:36 +0200

    refactor and comma globs

--- src/generate.ts
+++ src/generate.ts
@@ -12,75 +12,87 @@ import fs from "fs";
 import path from "path";
 import _ from "lodash";
 import glob from "glob";
+import { promisify } from "util";
 import { Config } from "./interfaces";
 
 interface MetadataSchemaObject {
   obj: SchemaObject
 }
 
-const buildSchemas = (fileglob, baseSchema): SchemaObject =>
-  new Promise((resolve, reject) => {
-    const root = process.cwd();
-    const schema = baseSchema;
-    glob(fileglob, {}, (err, files) => {
-      if (err) {
-        reject(err);
-      }
-      for (let file of files) {
-        const filename = path.join(root, file);
-        var stat = fs.lstatSync(filename);
-        if (!stat.isDirectory() && filename.indexOf(".js") >= 0) {
-          const apiSchema: SchemaObject = require(filename).default;
-          Object.entries(apiSchema).forEach(([key, value]) => {
-            if (value.id != key) {
-              apiSchema[value.id] = value;
-              delete apiSchema[key];
-            }
-          });
-          Object.assign(schema, apiSchema);
-        }
-      }
-      resolve(schema);
-    });
-  });
+const getJsLibs = async fileglobs => {
+  const asyncGlob = promisify(glob);
+  const root = process.cwd();
+  return []
+    .concat(
+      ...(await Promise.all(
+        fileglobs.split(",").map(fileglob => asyncGlob(fileglob, {}))
+      ))
+    )
+    .map(file => path.join(root, file))
+    .filter(filename => {
+      var stat = fs.lstatSync(filename);
+      return !stat.isDirectory() && filename.indexOf(".js") >= 0;
+    })
+    .map(file => require(file));
+};
 
-const buildControllers = (fileglob): Promise<Array<Function>> =>
-  new Promise((resolve, reject) => {
-    const root = process.cwd();
-    glob(fileglob, {}, (err, files) => {
-      if (err) {
-        reject(err);
+const buildSchemas = async (fileglobs, baseSchema): Promise<SchemaObject> => {
+  const schema = baseSchema;
+  for (let lib of await getJsLibs(fileglobs)) {
+    const apiSchema: SchemaObject = lib.default;
+    Object.entries(apiSchema).forEach(([key, value]) => {
+      if (value.id != key) {
+        apiSchema[value.id] = value;
+        delete apiSchema[key];
       }
-      resolve(
-        files
-          .map(file => path.join(root, file))
-          .map(filename => require(filename))
-          .map(imported => _.first(_.filter(_.values(imported), _.isFunction)))
-      );
     });
-  });
+    Object.assign(schema, apiSchema);
+  }
+  return schema;
+};
+
+const buildControllers = async (fileglobs): Promise<Array<Function>> =>
+  (await getJsLibs(fileglobs)).map(imported =>
+    _.first(_.filter(_.values(imported), _.isFunction))
+  );
 
 const generateCodeSamples = (
   spec: OpenAPIObject,
   config: Config["samples"]
 ) => {
-  const root = path.join(process.cwd(), config.dir)
-  _.forEach(_.entries(spec.paths), ([pathObj, methods]) => {
-      _.forEach(_.entries(methods), ([method, operation]) => {
-        spec.paths[pathObj][method]['x-code-samples'] = _.reject(
-          _.map(_.entries(config.languages), ([lang, {extension}]) => {
-            const filename = path.join(root, lang, `${operation.operationId}.${method}.${extension}`)
-            let source = ''
-            if (fs.existsSync(filename)) {
-                source = fs.readFileSync(filename).toString()
-            }
-            if (_.isEmpty(source)) { return null; }
-            return { lang, source };
-          }),
-          _.isEmpty
-        )
-      });
-  })
+  const root = path.join(process.cwd(), config.dir);
+  _.chain(_.entries(spec.paths))
+    .flatMap(([api, methods]) =>
+      _.map(_.entries(methods), ([method, operation]) => ({
+        api,
+        method,
+        operation
+      }))
+    )
+    .flatMap(descriptor =>
+      _.map(_.entries(config.languages), ([lang, langConfig]) =>
+        _.merge(descriptor, { lang, langConfig })
+      )
+    )
+    .map(descriptor => {
+      const filename = path.join(
+        root,
+        descriptor.lang,
+        `${descriptor.operation.operationId}.${descriptor.method}.${descriptor.langConfig.extension}`
+      );
+      const source = fs.existsSync(filename)
+        ? fs.readFileSync(filename).toString()
+        : null;
+      return _.merge(descriptor, { source });
+    })
+    .reject(descriptor => _.isEmpty(descriptor.source))
+    .map(({ api, method, source, lang }) => {
+      const samples = spec.paths[api][method]["x-code-samples"] || [];
+      spec.paths[api][method]["x-code-samples"] = samples.concat([
+        { lang, source }
+      ]);
+    })
+    .value();
 };
 
 const createParamType = (
